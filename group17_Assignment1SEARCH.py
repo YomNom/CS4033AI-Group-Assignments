f89ab36eb58f9481861fcf302ec5e477da518865
YomNom <joeyyyong@gmail.com>
2024-09-17 21:52:26 -0400

    Adding Files

---
+++ group17_Assignment1SEARCH.py
@@ -0,0 +1,13 @@
+# Group 17 - All members contributed in equal measure
+#	Member Names: Afreen Alam, Joey Yong, Khushbu Borwal
+# Honor Statement: 
+# Bibliography: 
+#	
+
+# FILE: Assignment 1 SEARCH
+# FALL 2024 CS 4033 Artificial Intelligence 
+# Purpose: Implementing informed & uninformed search of a part of Romania
+#	(1) Breadth Search
+#	(2) Depth First
+#	(3) Best First(greedy algorithm)
+#	(4) A* Algorithm
